a142c51b8ee7ae05d30445d580e04f7b2ce32a38
NoJuuq <154678291+NoJuuq@users.noreply.github.com>
2023-12-23 13:49:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('img');
+
+  img.style.background-image = "//";
+
+  // 👇️ optionally change text color
+  // box.style.color = 'white';
+});
